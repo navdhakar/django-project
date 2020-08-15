c6e50493418cc8ffa95426d6b6a5a16f7497aeae
navdhakar <navdepdhakar1@gmail.com>
2020-08-15 13:58:06 +0530

    added css  components from materialize

--- testsite/main/views.py
+++ testsite/main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tutorial
+
+
+
 
 def homepage(request):
-    return HttpResponse("<h1>django-project<h1>")
+    return render(request=request,
+    template_name="main/home.html",
+    context={"tutorials": Tutorial.objects.all})
 # Create your views here.
