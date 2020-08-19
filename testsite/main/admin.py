6169ecfbf53f28ac603853123052375a2100b5aa
navdhakar <navdepdhakar1@gmail.com>
2020-08-19 12:46:27 +0530

    model linked with foreign keys

--- testsite/main/admin.py
+++ testsite/main/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
+from django.db import models
 # Register your models here.
+
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
 admin.site.register(Tutorial)
+
